d832dbb71bfe6404bb03126ee8deb70a2643c0c7
bianqipeng <bqp18975203048@gmail.com>
2021-12-08 14:12:26 +0800

    查询菜单

--- src/main/java/com/example/sys/controller/MenuController.java
+++ src/main/java/com/example/sys/controller/MenuController.java
@@ -1,8 +1,13 @@
 package com.example.sys.controller;
 
+import com.example.common.domain.Resp;
+import com.example.sys.domain.MenuEntity;
 import com.example.sys.service.MenuService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * 菜单信息表
@@ -17,4 +22,9 @@ public class MenuController {
     @Autowired
     private MenuService menuService;
 
+    @GetMapping("/list")
+    public Resp<List<MenuEntity>> list() {
+        List<MenuEntity> menuEntityList = menuService.list();
+        return Resp.ok(menuEntityList);
+    }
 }
