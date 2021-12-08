41b8598b83afe2c273b5d3bbfca7c9f999d9ef69
bianqipeng <bqp18975203048@gmail.com>
2021-12-08 11:49:07 +0800

    查询角色

--- src/main/java/com/example/sys/controller/RoleController.java
+++ src/main/java/com/example/sys/controller/RoleController.java
@@ -1,8 +1,13 @@
 package com.example.sys.controller;
 
+import com.example.common.domain.Resp;
+import com.example.sys.domain.RoleEntity;
 import com.example.sys.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * 角色信息表
@@ -16,5 +21,11 @@ public class RoleController {
 
     @Autowired
     private RoleService roleService;
+
+    @GetMapping("/list")
+    public Resp<List<RoleEntity>> list() {
+        List<RoleEntity> roleEntityList = roleService.list();
+        return Resp.ok(roleEntityList);
+    }
 
 }
